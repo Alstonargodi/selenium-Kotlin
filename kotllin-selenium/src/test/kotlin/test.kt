23f919392a77d14b95988776b49cfbdb5624b7bc
alston <wayanalston185@gmail.com>
2025-02-02 21:36:40 +0700

    add: upload file

--- kotllin-selenium/src/test/kotlin/test.kt
+++ kotllin-selenium/src/test/kotlin/test.kt
@@ -1,6 +1,8 @@
 import org.openqa.selenium.By
 import org.openqa.selenium.WebDriver
+import org.openqa.selenium.WebElement
 import org.openqa.selenium.chrome.ChromeDriver
+import org.openqa.selenium.devtools.v129.filesystem.model.File
 import org.openqa.selenium.firefox.FirefoxDriver
 import java.sql.Driver
 import kotlin.test.BeforeTest
@@ -18,10 +20,23 @@ class test {
     fun openBrowser(){
         driver.get("https://www.selenium.dev/selenium/web/web-form.html")
 
+        setText()
+
+        uploadFile()
+    }
+
+    fun setText(){
         val title = driver.title
         println(title)
 
-        var textBox = driver.findElement(By.name("my-text"))
+        val textBox = driver.findElement(By.name("my-text"))
         textBox.sendKeys("aaa")
+    }
+
+    fun uploadFile(){
+        val nameFile : java.io.File = java.io.File("C:/Users/alston/software-project/katalon-project/miniproject-2/kotllin-selenium/resources/testupload.txt")
+        val fileInput : WebElement = driver.findElement(By.cssSelector("input[type=file]"))
+        fileInput.sendKeys(nameFile.absolutePath)
+        driver.findElement(By.id("file-submit")).click()
     }
 }
